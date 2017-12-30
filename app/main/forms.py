063b1a161991fc1918156409c2635e9ccebc0db1
ksc <ksc@mail.ustc.edu.cn>
2017-12-30 20:57:17 +0800

    crypto

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,17 @@
 # -*- coding:utf-8 -*-
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
-    SubmitField, FileField#, FileAllowed, FileRequired
+    SubmitField, FileField, PasswordField#, FileAllowed, FileRequired
 from wtforms.validators import Required, Length, Email, Regexp
 from wtforms import ValidationError
 from flask_pagedown.fields import PageDownField
 from ..models import Role, User
 
+
+class CryptoForm(FlaskForm):
+    password=StringField('Input a password', validators=[Required(),
+            Length(1,64)])
+    submit=SubmitField('Confirm')
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
